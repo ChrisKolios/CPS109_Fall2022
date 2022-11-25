14a186b15ce172ce043bc9e762bbcc7640d23708
ChrisKolios <91111093+ChrisKolios@users.noreply.github.com>
2022-11-24 23:04:50 -0500

    Updating problem description and metrics

--- Lab9/Quizzes/QuizFile.py
+++ Lab9/Quizzes/QuizFile.py
@@ -9,6 +9,12 @@ import GridClassFile
 # Note that the iguana moves first (then the snakes)
 # Return either "up", "down", "left", or "right"
 
+# To win, the iguana ('I') must reach the goal ('G')
+# The iguana will always spawn at the centre of the grid
+# Snakes ('S') will randomly spawn on the boundaries of the grid, as will the goal.
+# Snakes will always move in the direction of the iguana.
+# If a snake touches the iguana you lose ('X'). If the iguana touches the goal you win ('W').
+
 def generate_iguana_move(my_grid_environment):
     # Hints: you may find the following useful:
     
@@ -30,6 +36,7 @@ def generate_iguana_move(my_grid_environment):
 
 if __name__ == "__main__":
     print("Initializing escape simulation")
+    generate_metrics = False # Change this to True to generate your average performance!
 
     my_grid = GridClassFile.GridEnvironment(size=9, num_snakes=5, do_print=True)
 
@@ -69,27 +76,28 @@ if __name__ == "__main__":
     '''
 
     # Metrics generation
-    num_victories = 0
-    num_losses = 0
-    for i in range(1000):
-        my_grid = GridClassFile.GridEnvironment(size=9, num_snakes=5, do_print=False)
+    if (generate_metrics):
+        num_victories = 0
+        num_losses = 0
+        for i in range(1000):
+            my_grid = GridClassFile.GridEnvironment(size=9, num_snakes=5, do_print=False)
 
-        #my_grid.print_grid("start")
+            #my_grid.print_grid("start")
 
-        either_exit = False
-        while not either_exit:
-            
-            iguana_move = generate_iguana_move(my_grid)
-            my_grid.run_round(iguana_move)
+            either_exit = False
+            while not either_exit:
 
-            if (my_grid.iguana_escaped):
-                #print("The iguana has escaped! (win)")
-                num_victories += 1
-                either_exit = True
-            elif (my_grid.iguana_ded):
-                #print("The snakes are victorious... (loss)")
-                num_losses += 1
-                either_exit = True
-    print("Wins:", num_victories, "Losses:", num_losses, "Win Percent:", num_victories/num_losses)
+                iguana_move = generate_iguana_move(my_grid)
+                my_grid.run_round(iguana_move)
+
+                if (my_grid.iguana_escaped):
+                    #print("The iguana has escaped! (win)")
+                    num_victories += 1
+                    either_exit = True
+                elif (my_grid.iguana_ded):
+                    #print("The snakes are victorious... (loss)")
+                    num_losses += 1
+                    either_exit = True
+        print("Wins:", num_victories, "Losses:", num_losses, "Win Ratio:", num_victories/num_losses, "Win Percent:", round(num_victories/1000.0, 4))
     
     
